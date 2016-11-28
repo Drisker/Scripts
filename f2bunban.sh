1747430a36287a64dbed8a1cbe9e9ce8a2989da9
Drisker <drisker@outlook.com>
2016-11-28 03:22:26 +0100

    change user input prompt, clean up.

--- f2bunban.sh
+++ f2bunban.sh
@@ -1,19 +1,24 @@
 #!/bin/bash
 
-echo "Jail?"
+echo "=====Jail?====="
 read jail
-echo "IP?"
+echo "=====IP?====="
 read ip
 
 fail2ban-client set $jail unbanip $ip
 
 #this will append IP to be whitelisted to fail2ban config file at /etc/fail2ban/jail.local, as per f2b docs, this is the file that should be modified.
-echo "whitelist? y/n"
-read whitelist
 
-if [[ "whitelist" == "y" ]]
-# -i = infile edit , \b = zero-width word boundary , & = refer to that portion of the pattern. Refer to sed manual.
-then sed -i 's/\bignoreip = \b/&$ip /' /etc/fail2ban/jail.local
-else echo "Done"
+read -p "=====Whitelist (y/n)?=====" -n 1 -r
+echo
+if [[ "$REPLY" == "y" ]]
+then
+sed -i "s/\bignoreip = \b/&$ip /" /etc/fail2ban/jail.local
+echo "=====Done====="
+else
+echo
+echo "=====Done====="
 fi
+
+
 > /dev/null ;
